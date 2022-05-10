adca6a7c1f8c3430c9a97b36aad65cf48d795b7d
mahdi2357 <mahdi.mehdizade80@gmail.com>
2022-05-10 12:10:13 +0430

    a function added

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ def hello():
 
 print(fibonachi(10))
 hello()
+
+# create a function that takes a string and returns a string with the first and last characters swapped
+def swap_first_last_characters(word):
+    return word[-1] + word[1:-1] + word[0]
+    
