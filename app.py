87b16f45f48664b1ac057d10afddea043462ec5b
mahdi2357 <mahdi.mehdizade80@gmail.com>
2022-05-11 16:41:38 +0430

    some changes

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ hello()
 # create a function that takes a string and returns a string with the first and last characters swapped
 def swap_first_last_characters(word):
     return word[-1] + word[1:-1] + word[0]
-    
+
+
+# some changes here
